ade136772934121b5e8ddbebc53e4a62e2a5e97d
FlavioSabino <35316216+FlavioSabino@users.noreply.github.com>
2022-08-22 23:14:19 -0300

    app calculadora de notas

---
+++ app/src/main/java/com/flavio/calculadoradenotas/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.flavio.calculadoradenotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flavio.calculadoradenotas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btCalcular.setOnClickListener {
+            calcularMedia()
+        }
+    }
+    fun calcularMedia(){
+        val num1 = binding.nota1
+        val num2 = binding.nota2
+        val num3 = binding.nota3
+        val num4 = binding.nota4
+        val faltas = binding.faltas
+        val resultado = binding.txtResultado
+
+        val nota1 = Integer.parseInt(num1.text.toString())
+        val nota2 = Integer.parseInt(num2.text.toString())
+        val nota3 = Integer.parseInt(num3.text.toString())
+        val nota4 = Integer.parseInt(num4.text.toString())
+        val numeroFaltas = Integer.parseInt(faltas.text.toString())
+
+        val media = (nota1 + nota2 + nota3 + nota4) / 4
+
+        if (media >=5 && numeroFaltas <=20){
+            resultado.setText("Aluno foi aprovado \n Média final $media")
+            resultado.setTextColor(getColor(R.color.green_700))
+        }else if(numeroFaltas >20 && media <5) {
+            resultado.setText("Aluno foi Reprovado por faltas e por nota \n Média final $media")
+            resultado.setTextColor(getColor(R.color.red))
+
+        }else if (numeroFaltas >20){
+            resultado.setText("Aluno foi Reprovado por faltas \n Média final $media")
+            resultado.setTextColor(getColor(R.color.red))
+        }else if(media <5){
+            resultado.setText("Aluno foi Reprovado por Nota \n Média final $media")
+            resultado.setTextColor(getColor(R.color.red))
+        }
+    }
+}
